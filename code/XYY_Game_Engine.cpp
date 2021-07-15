2b119a0eff47bb93cfcbf4e88d8ecb98087ac78a
cuiwenyao <yao1970099540@163.com>
2021-07-15 11:21:14 +0800

    del

--- code/XYY_Game_Engine.cpp
+++
@@ -1,6 +0,0 @@
-//XYY_Game_Engine.cpp
-#include "XYY_Game_Engine.h"
-extern "C" _declspec(dllexport)void start()
-{
-	;
-}
--- code/XYY_Game_Engine.h
+++
@@ -1,5 +0,0 @@
-//XYY_Game_Engine.h
-#ifndef XYY_Game_Engine_H
-#define XYY_Game_Engine_H
-extern "C" _declspec(dllexport) void start();
-#endif
